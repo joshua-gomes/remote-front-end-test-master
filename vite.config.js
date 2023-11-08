e180701546e8f147be69e93f4167afeb444eeb97
Joshimoto <joshuagomes97@gmail.com>
2023-11-08 23:24:39 +0000

    Setup env variable for API_URL

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    console.log({ env });
+
+    return {
+        plugins: [react()],
+        define: {
+            'import.meta.env.API_URL': JSON.stringify(env.API_URL),
+        },
+    };
 });
